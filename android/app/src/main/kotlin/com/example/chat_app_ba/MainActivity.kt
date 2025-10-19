89ca9cb78da10a208a2d1aff1dc2131c1e84e851
zayed-rahat <zayedrahat088@gmail.com>
2025-10-19 11:32:36 +0600

    project setup

---
+++ android/app/src/main/kotlin/com/example/chat_app_ba/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chat_app_ba
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BA Chat',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          elevation: 2,
+          iconTheme: IconThemeData(color: Colors.green),
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
